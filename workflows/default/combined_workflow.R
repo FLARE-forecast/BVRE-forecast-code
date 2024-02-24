035e91b714b59aa653aca3f4f7ae912f15237985
rqthomas <rqthomas@vt.edu>
2024-02-24 08:00:23 -0500

    cleaning up

--- workflows/default/combined_workflow.R
+++ workflows/default/combined_workflow.R
@@ -1,11 +1,14 @@
 library(tidyverse)
 library(lubridate)
+
 lake_directory <- here::here()
 setwd(lake_directory)
-forecast_site <<- "bvre"
-configure_run_file <<- "configure_run.yml"
-update_run_config <<- TRUE
-config_set_name <<- "default"
+forecast_site <- "bvre"
+configure_run_file <- "configure_run.yml"
+config_set_name <- "default"
+
+files.sources <- list.files(file.path(lake_directory, "R"), full.names = TRUE)
+sapply(files.sources[grepl(".R$", files.sources)], source)
 
 Sys.setenv("AWS_DEFAULT_REGION" = "renc",
            "AWS_S3_ENDPOINT" = "osn.xsede.org",
@@ -17,20 +20,68 @@ noaa_ready <- TRUE
 
 while(noaa_ready){
     
-  message("Generating targets")
-  source(file.path("workflows", config_set_name, "01_generate_targets.R"))
+  message("Beginning generate targets")
+
+  config_obs <- FLAREr::initialize_obs_processing(lake_directory, observation_yml = "observation_processing.yml", config_set_name = config_set_name)
+  config <- FLAREr::set_configuration(configure_run_file,lake_directory, config_set_name = config_set_name)
   
-  setwd(lake_directory)
   
-  message("Generating inflow forecast")
-  source(file.path("workflows", config_set_name, "02_run_inflow_forecast.R"))
+  dir.create(file.path(lake_directory, "targets", config_obs$site_id), showWarnings = FALSE)
+
+  FLAREr::get_git_repo(lake_directory,
+                       directory = config_obs$realtime_insitu_location,
+                       git_repo = "https://github.com/FLARE-forecast/BVRE-data.git")
   
-  setwd(lake_directory)
+  FLAREr::get_edi_file(edi_https = "https://pasta.lternet.edu/package/data/eml/edi/725/3/a9a7ff6fe8dc20f7a8f89447d4dc2038",
+                       file = config_obs$insitu_obs_fname[2],
+                       lake_directory)
   
-  message("Generating forecast")
-  source(file.path("workflows", config_set_name, "03_run_flarer_forecast.R"))
+  FLAREr::get_edi_file(edi_https = "https://pasta.lternet.edu/package/data/eml/edi/725/3/5927a50118644fa451badb3b84233bb7",
+                       file = config_obs$insitu_obs_fname[3],
+                       lake_directory)
   
-  setwd(lake_directory)
+  cleaned_insitu_file <- in_situ_qaqc(insitu_obs_fname = file.path(lake_directory,"data_raw", config_obs$insitu_obs_fname),
+                                      data_location = file.path(lake_directory,"data_raw"),
+                                      maintenance_file = file.path(lake_directory, "data_raw", config_obs$maintenance_file),
+                                      ctd_fname = NA,
+                                      nutrients_fname =  NA,
+                                      secchi_fname = NA,
+                                      cleaned_insitu_file = file.path(lake_directory,"targets", config_obs$site_id, paste0(config_obs$site_id,"-targets-insitu.csv")),
+                                      site_id = config_obs$site_id,
+                                      config = config_obs)
+  
+  FLAREr::put_targets(site_id = config_obs$site_id,
+                      cleaned_insitu_file,
+                      cleaned_met_file = NA,
+                      cleaned_inflow_file = NA,
+                      use_s3 = config$run_config$use_s3,
+                      config = config)
+  
+  # Run FLARE
+  output <- FLAREr::run_flare(lake_directory = lake_directory,
+                              configure_run_file = configure_run_file,
+                              config_set_name = config_set_name)
+  
+  
+  forecast_start_datetime <- lubridate::as_datetime(config$run_config$forecast_start_datetime) + lubridate::days(1)
+  start_datetime <- lubridate::as_datetime(config$run_config$forecast_start_datetime) - lubridate::days(1)
+  restart_file <- paste0(config$location$site_id,"-", (lubridate::as_date(forecast_start_datetime)- days(1)), "-",config$run_config$sim_name ,".nc")
+  
+  FLAREr::update_run_config2(lake_directory = lake_directory,
+                             configure_run_file = configure_run_file, 
+                             restart_file = restart_file, 
+                             start_datetime = start_datetime, 
+                             end_datetime = NA, 
+                             forecast_start_datetime = forecast_start_datetime,  
+                             forecast_horizon = config$run_config$forecast_horizon,
+                             sim_name = config$run_config$sim_name, 
+                             site_id = config$location$site_id,
+                             configure_flare = config$run_config$configure_flare, 
+                             configure_obs = config$run_config$configure_obs, 
+                             use_s3 = config$run_config$use_s3,
+                             bucket = config$s3$warm_start$bucket,
+                             endpoint = config$s3$warm_start$endpoint,
+                             use_https = TRUE)
     
   RCurl::url.exists("https://hc-ping.com/8b5c849d-a5a6-4d44-980c-676472cf3c70", timeout = 5)
   
