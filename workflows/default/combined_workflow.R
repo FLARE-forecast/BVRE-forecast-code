6654908f93124f1a70ca38e0fa0a8838a299a230
addelany <addelany@gmail.com>
2024-10-15 12:15:09 -0500

    add additional config statement

--- workflows/default/combined_workflow.R
+++ workflows/default/combined_workflow.R
@@ -22,8 +22,9 @@ FLAREr::ignore_sigpipe()
 
 message("Beginning generate targets")
 
-config_obs <- FLAREr::initialize_obs_processing(lake_directory, observation_yml = "observation_processing.yml", config_set_name = config_set_name)
+config_obs <- FLAREr:::initialize_obs_processing(lake_directory, observation_yml = "observation_processing.yml", config_set_name = config_set_name)
 config <- FLAREr::set_configuration(configure_run_file,lake_directory, config_set_name = config_set_name)
+#config <- FLAREr:::set_up_simulation(configure_run_file,lake_directory, config_set_name = config_set_name)
 
 
 dir.create(file.path(lake_directory, "targets", config_obs$site_id), showWarnings = FALSE)
@@ -61,6 +62,8 @@ noaa_ready <- TRUE
 
 while(noaa_ready){
   
+  config <- FLAREr::set_configuration(configure_run_file,lake_directory, config_set_name = config_set_name)
+  
   # Run FLARE
   output <- FLAREr::run_flare(lake_directory = lake_directory,
                               configure_run_file = configure_run_file,
@@ -72,16 +75,16 @@ while(noaa_ready){
   restart_file <- paste0(config$location$site_id,"-", (lubridate::as_date(forecast_start_datetime)- days(1)), "-",config$run_config$sim_name ,".nc")
   
   FLAREr::update_run_config2(lake_directory = lake_directory,
-                             configure_run_file = configure_run_file, 
-                             restart_file = restart_file, 
-                             start_datetime = start_datetime, 
-                             end_datetime = NA, 
-                             forecast_start_datetime = forecast_start_datetime,  
+                             configure_run_file = configure_run_file,
+                             restart_file = restart_file,
+                             start_datetime = start_datetime,
+                             end_datetime = NA,
+                             forecast_start_datetime = forecast_start_datetime,
                              forecast_horizon = config$run_config$forecast_horizon,
-                             sim_name = config$run_config$sim_name, 
+                             sim_name = config$run_config$sim_name,
                              site_id = config$location$site_id,
-                             configure_flare = config$run_config$configure_flare, 
-                             configure_obs = config$run_config$configure_obs, 
+                             configure_flare = config$run_config$configure_flare,
+                             configure_obs = config$run_config$configure_obs,
                              use_s3 = config$run_config$use_s3,
                              bucket = config$s3$warm_start$bucket,
                              endpoint = config$s3$warm_start$endpoint,
