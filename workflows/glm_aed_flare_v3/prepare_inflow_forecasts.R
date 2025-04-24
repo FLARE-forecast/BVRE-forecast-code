c039a0fdb4c886ff36c235c8948368c5fea81012
addelany <addelany@gmail.com>
2025-04-24 14:27:10 -0500

    update endpoint

--- workflows/glm_aed_flare_v3/prepare_inflow_forecasts.R
+++ workflows/glm_aed_flare_v3/prepare_inflow_forecasts.R
@@ -14,7 +14,7 @@ forecast_date <- config$run_config$forecast_start_datetime
 
 
 inflow_s3 <- arrow::s3_bucket(bucket = glue::glue("bio230121-bucket01/vera4cast/forecasts/parquet/project_id=vera4cast/duration=P1D/"),
-                       endpoint_override = "https://renc.osn.xsede.org",
+                       endpoint_override = "https://amnh1.osn.mghpcc.org",
                        anonymous = TRUE)
 
 inflow_df <- arrow::open_dataset(inflow_s3) |> 
